a37ed5a1598f528a07a59edf32731598f3cadb40
oosayeroo <98313914+oosayeroo@users.noreply.github.com>
2025-04-20 15:09:14 +0100

    Add files via upload

--- config.lua
+++ config.lua
@@ -2,12 +2,12 @@ Config = {}
 
 Config.DebugCode = false
 
-Config.ServerName = "Outback Zombies"
+Config.ServerName = "Your Server Name"
 
 --these are fallbacks incase you forget(or dont need to) fill these fields in
-Config.DefaultTitle = "Webhook" --just a fallback incase you forget to add a title to the webhook event
-Config.DefaultAuthorName = "Outback Zombies"
-Config.DefaultAuthorIconUrl = 'https://cdn.discordapp.com/attachments/1194864528186421330/1196330413648531496/outbackk.png?ex=6805dc85&is=68048b05&hm=e4b792342bc32071804da5d4f9f518a58b4a70fa69ed85b73f8cd04662eb5d19&'
+Config.DefaultTitle = "Sayer Logging" --just a fallback incase you forget to add a title to the webhook event
+Config.DefaultAuthorName = "Sayer Logs"
+Config.DefaultAuthorIconUrl = 'https://cdn.discordapp.com/attachments/1061012675112476672/1310542947581825074/OOSAYEROO_SCRIPTS.png?ex=680614bb&is=6804c33b&hm=8e7780e78b1d028a60bed50a7c126b8a4718afbcf81abb69377611ef7a18d5a7&'
 
 Config.UseLogQueue = false --waits until there is a certain amount in the queue before posting them / if false posts them straight away
 Config.LogQueueLimit = 5 --if using log queue then waits until this many logs are queued before posting them
@@ -18,8 +18,8 @@ Config.TaggableRoles = { --can add the field tagRoles into the data to tag speci
     ['mod'] = '1363510691461337248',
 }
 
-Config.Webhooks = { --can have as many webhooks as you like
-    ['default'] = 'https://discord.com/api/webhooks/1070875071113080862/oXKGTB1WlxI_QVTPkuIfjUQuaOSWkQkH6vwb7I6gT4vMhZV32c8ItDeJYFSkgvsLsRXy', --do not remove default field. it is used as a fallback incase you misspell a hook_id
+Config.Webhooks = { --can have as many webhooks as you like (place your links)
+    ['default'] = 'changeme', --do not remove default field. it is used as a fallback incase you misspell a hook_id
     ['loot'] = '',
     ['death'] = '',
     [''] = '',
